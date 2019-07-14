c19578704bd4f5aa5b90e3b89a7a7106dada22ab
Ismael Lopez <ismaellopez@Ismaels-MBP.fios-router.home>
2019-07-14 18:59:15 -0400

    hdhd

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
@@ -14,9 +15,9 @@ app.use(bodyParser.urlencoded({limit: '10mb', extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+  app.use(express.static("app"));
+// }
 
 // Define API routes here
 
