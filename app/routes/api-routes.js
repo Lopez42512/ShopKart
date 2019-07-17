9a5ffcbdd9adaeb119d56f88a90c9fadb740f804
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2019-07-17 15:47:50 -0400

    login almost done

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -31,13 +31,13 @@ module.exports = function(app) {
     console.log(req.body);
 
     db.Item.create({
-      UserId: req.body.userId,
       name: req.body.name,
       image: req.body.image,
       category: req.body.category,
       description: req.body.description,
       price: req.body.price,
-      create_at: req.body.create_at
+      create_at: req.body.create_at,
+      UserId: req.body.UserId
       
     }).then(function(results){
       res.end()
@@ -63,7 +63,7 @@ module.exports = function(app) {
   });
 
   //==============================++=======================+==+=====+=+=+====
-
+  var user_id_status;
   //========================== User login route ============================
   app.get("/api/username:n/Password:p", function(req, res) {
     console.log(req.params);
@@ -74,86 +74,22 @@ module.exports = function(app) {
         password: req.params.p
       }
     }).then(function(result) {
-      res.json(result);
+      // console.log(result);
+      
+      if(result === null){
+        user_id_status = false;
+       res.json(result);
+        
+      } else{
+        user_id_status = true;
+        console.log(user_id_status);
+      res.json(result.id);
+      }
     });
   });
-
+console.log("============================================================================================");
+console.log(user_id_status);
   
 
-  // // Get a specific book
-  // app.get("/api/:book", function(req, res) {
-  //   Book.findAll({
-  //     where: {
-  //       title: req.params.book
-  //     }
-  //   }).then(function(results) {
-  //     res.json(results);
-  //   });
-  // });
-
-  // Get all books from a specific author
-  //   app.get("/api/author/:author", function(req, res) {
-  //     Book.findAll({
-  //       where: {
-  //         author: req.params.author
-  //       }
-  //     }).then(function(results) {
-  //       res.json(results);
-  //     });
-  //   });
-
-  //   // Get all "long" books (books 150 pages or more)
-  //   app.get("/api/books/long", function(req, res) {
-  //     Book.findAll({
-  //       where: {
-  //         pages: {
-  //           $gte: 150
-  //         }
-  //       },
-  //       order: [["pages", "DESC"]]
-  //     }).then(function(results) {
-  //       res.json(results);
-  //     });
-  //   });
-
-  //   // Get all "short" books (books 150 pages or less)
-  //   app.get("/api/books/short", function(req, res) {
-  //     Book.findAll({
-  //       where: {
-  //         pages: {
-  //           $lte: 150
-  //         }
-  //       },
-  //       order: [["pages", "ASC"]]
-  //     }).then(function(results) {
-  //       res.json(results);
-  //     });
-  //   });
-
-  //   // Add a book
-  //   app.post("/api/new", function(req, res) {
-  //     console.log("Book Data:");
-  //     console.log(req.body);
-  //     Book.create({
-  //       title: req.body.title,
-  //       author: req.body.author,
-  //       genre: req.body.genre,
-  //       pages: req.body.pages
-  //     }).then(function(results) {
-  //       res.json(results);
-  //     });
-  //   });
-
-  //   // Delete a book
-  //   app.delete("/api/book/:id", function(req, res) {
-  //     console.log("Book ID:");
-  //     console.log(req.params.id);
-  //     Book.destroy({
-  //       where: {
-  //         id: req.params.id
-  //       }
-  //     }).then(function() {
-  //       res.end();
-  //     });
-  //   });
+  
 };
